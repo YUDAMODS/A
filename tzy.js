ed931bc3c1a9e9d6a0eba7f25f85efc7e5c01a42
YUDAMODS <119145280+YUDAMODS@users.noreply.github.com>
2024-04-25 16:29:34 +0700

    Add files via upload

---
+++ tzy.js
@@ -0,0 +1,19 @@
+/*SC BUG NEW TELE
+BASE : HW MODS
+RECODE : YudaMods
+CREACOT : YudaMods
+*/
+
+const settings = {
+  token: '7085058799:AAFXC-7U5gCLpkBeTDMgr2Zqv4-x4VCrbkg',// Token Bot panel dan domain
+  adminId: '5887546516',
+  urladmin: 'https://t.me/YUDAMODS',
+  domain: 'https://buyer.paneellkuu.my.id', // Isi dengan domain yang digunakan
+  plta: 'ptla_bFajnTGxM9t4Nm43I44ETwgkIHWTcBJPZho2ky11xyC', // Isi dengan nilai plta yang sesuai
+  pltc: 'ptlc_t7YnRn2AfWc9yXO4kJmaz0t1tTQE4Sj3ZBdU9EECjQK', // Isi dengan nilai pltc yang sesuai
+  pp: 'https://telegra.ph/file/2c9967f6cc7b9ad4a7adc.jpg',
+  loc: '1', // Isi dengan lokasi yang diinginkan
+  eggs: '15'
+};
+
+module.exports = settings;
